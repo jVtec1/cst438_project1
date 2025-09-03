13223ac4537e7af70da8c8ecca4c44d0b1c6b225
jeslynsee <jeslynesee@gmail.com>
2025-09-02 22:36:40 -0700

    Add verification to username and password length. Display error message if length requirment violated

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,43 @@
 import { Text, TextInput, View, Button } from "react-native";
-import { Stack }from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
 
 export default function Index() { // did not rename file to login-page because already routes to index page/first page
+  // need to use useState to capture user inputs 
+  const [usernameInput, setUsername] = useState(""); 
+  const [passwordInput, setPassword] = useState("");
+  const [usernameErrorMsg, setUserErrorMsg] = useState("");
+  const [passwordErrorMsg, setPassErrorMsg] = useState("");
+
+  const router = useRouter(); // need this router to change pages, e.g.: from login to landing page
+
+
+  const verifyLogin = () => {
+    console.log("Verify login function working!"); 
+    if (usernameInput.length < 2) {
+      setUserErrorMsg("Username has to be 2 or more characters in length.")
+    } else if (usernameInput.length >= 2) {
+      setUserErrorMsg("");
+    }
+
+    if (passwordInput.length < 3) {
+      setPassErrorMsg("Password has to be 3 or more characters in length.")
+    } else if (passwordInput.length >= 3) {
+      setPassErrorMsg("");
+    }
+
+    // error check with database, ensure user is in db and username and password matches
+    // use router.push(route-name) here to take user from login to landing page
+  };
+
+  const directToSignUp = () => {
+    console.log("Setting up sign up page soon!");
+  }
+
   return (
     <> 
     <Stack.Screen options={{title:"Login"}}/> {/* had to access Stack to change title of page from index to Login */}
-    <View // wihtout other View (parent), code gets error
+    <View // without other View (parent), code gets error
     style={{
       justifyContent: "center",
       alignItems: "center",
@@ -20,22 +52,26 @@ export default function Index() { // did not rename file to login-page because a
       >
       
     <Text style={{padding: 10}}> Username: </Text>
-    <TextInput placeholder="Enter username" style={{borderWidth: 1}}/>
+    <TextInput placeholder="Enter username" style={{borderWidth: 1}} value={usernameInput} onChangeText={setUsername}/>
      
-      </View>
+    </View>
+
+    <Text style={{color: "red"}}> {usernameErrorMsg} </Text>
 
       <View
       style={{
         flexDirection: "row",
       }}>
       <Text style={{padding: 10}}> Password: </Text>
-      <TextInput placeholder="Enter password" style={{borderWidth: 1}}/>
+      <TextInput placeholder="Enter password" style={{borderWidth: 1}} value={passwordInput} onChangeText={setPassword}/>
       </View>
 
-    <Button title="Sign in" onPress={() => console.log("Button check!")}/>
+    <Text style={{color: "red"}}> {passwordErrorMsg} </Text>
 
-    <Text> Don&apos;t have an account? Click here: </Text>
-    <Button title="Sign up" onPress={() => console.log("take user to sign up page!")}/>
+    <Button title="Sign in" onPress={verifyLogin}/>
+
+    <Text style={{marginTop: 20}}> Don&apos;t have an account? Click here: </Text>
+    <Button title="Sign up" onPress={directToSignUp}/>
 
     </View>
     </>
