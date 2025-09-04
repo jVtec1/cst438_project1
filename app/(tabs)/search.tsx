95492bc96bd01a43437ab43bad90cbfcb0852956
jeslynsee <jeslynesee@gmail.com>
2025-09-04 13:22:14 -0700

    Create basic text layout of search vehicles page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -2,14 +2,35 @@ import { Text, View } from "react-native";
 
 export default function Search() {
   return (
+    <View>
+
     <View
       style={{
-        flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/(tabs)/search.tsx to edit this screen.</Text>
+      <Text>Search for Vehicles</Text>
+    </View>
+
+ {/* start of dropdown menus section */}
+    <View style={{justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20
+        }}>
+
+    <View style={{flexDirection: "row"}}>
+      <Text style={{padding: 10}}> Make: </Text>
+    </View>
+
+    <View style={{flexDirection: "row"}}>
+      <Text style={{padding: 10}}> Model: </Text>
+    </View>
+
+    </View>
+    {/* end of dropdown menus section */}
+
+
     </View>
   );
 }
