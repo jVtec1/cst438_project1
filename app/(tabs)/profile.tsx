c9b1bf9821db72fca55de166405c44f8009a6f29
SpIurge <sergonzalez@csumb.edu>
2025-09-04 12:48:03 -0700

    made changes

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
-
-export default function Profile() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/(tabs)/profile.tsx to edit this screen.</Text>
-    </View>
-  );
+import {StyleSheet, Text, View, Image} from 'react-native'
+const infoPage = () => {
+    return (
+        <View style = {styles.container}>
+            <Text style = {styles.title}>Personal Info</Text>
+            <Text style ={{marginTop: 10, marginBottom: 30}}>Name:</Text>
+            <Text style ={{marginTop: 10, marginBottom: 30}}>Address:</Text>
+            <Text style ={{marginTop: 10, marginBottom: 30}}>Credits:</Text>
+            <View>
+                <Text style = {styles.card}>Edit Profile</Text>
+            </View>
+        </View>
+    )
 }
+
+export default infoPage
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        alignItems: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    card:{
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 6,
+        boxShadow: '5px 5px rgba(0,0,0,0.1)'
+    }
+})
