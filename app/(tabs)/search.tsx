14accf04fb0ffe334cf5da4f2054ee47c04ce6a9
jeslynsee <jeslynesee@gmail.com>
2025-09-09 20:51:39 -0700

    Complete dropdown menu and search button for search page. Need to connect to Sergio's RecyclerView logic next

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,13 +1,11 @@
 //@ts-nocheck
 // above helps with removing typescript-related errors while writing in JavaScript on tsx file
-import { Text, View, Alert } from "react-native";
+import { Text, View, Alert, Button } from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
 import { useState, useEffect } from "react";
 import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 export default function Search() {
-
-  //TODO NEXT: Grab user's make and model choices, already saved from useState, and put them in params in API call
 
   const [carMakes, setCarMakes] = useState([]);
   const [carModels, setCarModels] = useState([]);
@@ -18,7 +16,7 @@ export default function Search() {
       const response = await fetch("https://www.auto.dev/api/models", {
         method: "GET",
         headers: {
-          Authorization: "Bearer sk_ad_XQYDCGKi5ed3CiT_Pf_GnrGw", // replace with your key
+          Authorization: "Bearer sk_ad_XQYDCGKi5ed3CiT_Pf_GnrGw", 
           "Content-Type": "application/json",
         },
       });
@@ -51,7 +49,7 @@ export default function Search() {
 
    setSelectedMake(make.value); // have to assign make.value, not just "make" because make is whole Object (e.g. {label: "", value: ""})
    setSelectedModel(null); // ensuring if user changes mind about make, models don't still show previous ones
-   Alert.alert("You chose: " + make.label);
+   Alert.alert("You chose: " + make.label); // alert used to make sure values being captured 
 
    try {
 
@@ -70,16 +68,23 @@ export default function Search() {
 
   const onHandleChangeModel = (model) => {
     setSelectedModel(model.value); // saves our model value 
-    Alert.alert("You chose this model: " + model.value);
+    Alert.alert("You chose this model: " + model.value); // alert used to make sure values being captured 
+  }
+
+  // function for after button click; TODO: Connect this to Recycler View Sergio working on
+  // grab captured selectedMake and selectedModel and put them as params into API Call
+  const captureMakeAndModel = () => {
+    Alert.alert("You selected: " + selectedMake + " " + selectedModel); // alert used to make sure values being captured 
   }
   
 
   const [selectedMake, setSelectedMake] = useState(null); // saves current selected make and even shows in dropdown bar
-  const [selectedModel, setSelectedModel] = useState(null); // saaves current selected model and even shows in dropdown bar
-  // const [modelData, setModelData] = useState([]);
+  const [selectedModel, setSelectedModel] = useState(null); // saves current selected model and even shows in dropdown bar
 
   return (
-    <View>
+    <View style={{
+      marginTop: 20
+    }}>
 
     <View
       style={{
@@ -101,11 +106,11 @@ export default function Search() {
     }}>
       <Text style={{padding: 10}}> Make: </Text>
       <Dropdown style={{
-        height: 50,           // ← Fixed height
+        height: 50,          
         borderWidth: 1, 
         borderColor: "#ccc",
         borderRadius: 8,
-        paddingHorizontal: 15, // ← Padding for text
+        paddingHorizontal: 15, 
         backgroundColor: 'white',
         width: "25%"
     }} 
@@ -122,11 +127,11 @@ export default function Search() {
     }}>
       <Text style={{padding: 10}}> Model: </Text>
       <Dropdown style={{
-        height: 50,           // ← Fixed height
+        height: 50,           
         borderWidth: 1, 
         borderColor: "#ccc",
         borderRadius: 8,
-        paddingHorizontal: 15, // ← Padding for text
+        paddingHorizontal: 15, 
         backgroundColor: 'white',
         width: "25%"
     }} 
@@ -140,6 +145,19 @@ export default function Search() {
 
     </View>
     {/* end of dropdown menus section */}
+
+    <View 
+    style={{
+      justifyContent: "center",
+      alignItems: "center",
+      length: "25%",
+      marginTop: 20
+    }}>
+    <Button title="Search"
+    onPress={captureMakeAndModel}
+    />
+
+    </View>
 
 
     </View>
