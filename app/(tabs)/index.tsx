3148a20bf6f82a17ab9960ae43c8a1343f69c8c5
SpIurge <sergonzalez@csumb.edu>
2025-09-18 13:31:48 -0700

    lilguys

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        alignContent:"center",
+        backgroundColor:"#005EB8",
       }}
     >
-      <Text>Edit app/(tabs)/index.tsx to edit this screen.</Text>
+      <Text style={{fontSize:20,padding:10,color:"gold",textAlign:"center"}}>Welcome to Monte's Auto Dealership!</Text>
+      <Image source={require('../../assets/images/lilguy.png')} style={{width:400, height:600, alignItems:"flex-end"}}/>
+
+
+
     </View>
   );
 }
