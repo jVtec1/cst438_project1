e6f0c8694c4809bc5e73204d9a461183ef09acf4
SpIurge <sergonzalez@csumb.edu>
2025-09-28 19:43:25 -0700

    Final Change. Added animation FontChange Format

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,85 @@
-import { Text, View, Image } from "react-native";
+import React, { useRef, useEffect } from "react";
+import { Text, Image, StyleSheet, Animated } from "react-native";
+import AppLoading from "expo-app-loading";
+import {useFonts, Inter_400Regular,Inter_600SemiBold} from "@expo-google-fonts/inter";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+  });
+
+  // Creates animation for wow factor
+  const fadeAnimation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      Animated.timing(fadeAnimation, {
+        toValue: 1,
+        duration: 1000, //1 second
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignContent:"center",
-        backgroundColor:"#005EB8",
-      }}
-    >
-      <Text style={{fontSize:20,padding:10,color:"gold",textAlign:"center"}}>Welcome to Monte's Auto Dealership!</Text>
-      <Image source={require('../../assets/images/lilguy.png')} style={{width:400, height:600, alignItems:"flex-end"}}/>
+    <Animated.View style={[styles.container, { opacity: fadeAnimation }]}>
+      <Text style={styles.header}>
+        Welcome to Monte's Auto Dealership!
+      </Text>
 
+      <Image
+        source={require("../../assets/images/lilguy.png")}
+        style={styles.image}
+      />
 
+      <Text style={styles.subText}>
+        The only place you will find the best deals!
+      </Text>
 
-    </View>
+      <Text style={styles.sponsorText}>
+        Proudly Sponsored by Auto.Dev. The all-in-one place for your API dealership needs.
+      </Text>
+    </Animated.View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#005EB8",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  header: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 24,
+    color: "gold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  image: {
+    width: "100%",
+    height: "50%",
+    resizeMode: "contain",
+    marginBottom: 20,
+  },
+  subText: {
+    fontFamily: "Inter_400Regular",
+    fontSize: 18,
+    color: "gold",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  sponsorText: {
+    fontFamily: "Inter_400Regular",
+    fontSize: 16,
+    color: "gold",
+    textAlign: "center",
+  },
+});
